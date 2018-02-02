57385366a07f0d30af2a00f3363496c96c8d9095
muzi <727784430@qq.com>
2018-02-02 13:02:07 +0800

    logo  欢迎页

--- app/src/main/java/com/zy/yaoproject/ui/WelcomeActivity.java
+++ app/src/main/java/com/zy/yaoproject/ui/WelcomeActivity.java
@@ -1,23 +1,16 @@
 package com.zy.yaoproject.ui;
 
-import android.widget.ImageView;
-
 import com.zy.yaoproject.R;
 import com.zy.yaoproject.base.activity.BaseActivity;
 import com.zy.yaoproject.task.TypeFaceTask;
 import com.zy.yaoproject.ui.main.MainActivity;
-import com.zy.yaoproject.utils.ImagLoadUtils;
 
 import java.util.concurrent.TimeUnit;
 
-import butterknife.BindView;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 
 public class WelcomeActivity extends BaseActivity {
-
-    @BindView(R.id.welcome_img)
-    ImageView welcomeImg;
 
     @Override
     protected int bindLayout() {
@@ -26,7 +19,6 @@ public class WelcomeActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        ImagLoadUtils.loadImg(this, R.drawable.welcome_img, welcomeImg);
         initIconFont();
     }
 
@@ -36,7 +28,10 @@ public class WelcomeActivity extends BaseActivity {
         typeFaceTask.setComplete(() ->
                 Observable.timer(500, TimeUnit.MILLISECONDS)
                         .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(aLong -> startActivity(MainActivity.class))
+                        .subscribe(aLong -> {
+                            startActivity(MainActivity.class);
+                            finish();
+                        })
         );
     }
 }
