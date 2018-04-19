eaa60b5d51cb06df4838464c5dd9b38179a71f3a
muzi <727784430@qq.com>
2018-04-19 18:34:33 +0800

    tag

--- app/src/main/java/com/zy/yaoproject/adapter/LogisticsChildAdapter.java
+++ app/src/main/java/com/zy/yaoproject/adapter/LogisticsChildAdapter.java
@@ -1,5 +1,6 @@
 package com.zy.yaoproject.adapter;
 
+import android.graphics.Color;
 import android.support.annotation.Nullable;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -7,6 +8,7 @@ import com.chad.library.adapter.base.BaseViewHolder;
 import com.zy.yaoproject.R;
 import com.zy.yaoproject.bean.LogisticsBean;
 import com.zy.yaoproject.utils.DateUtils;
+import com.zy.yaoproject.widget.TagView;
 
 import java.util.List;
 
@@ -22,15 +24,18 @@ public class LogisticsChildAdapter extends BaseQuickAdapter<LogisticsBean.DataBe
 
     @Override
     protected void convert(BaseViewHolder helper, LogisticsBean.DataBeanX.DataBean item) {
-        String time = DateUtils.stampToDate(String.valueOf(item.getCreateDate()));
+        String time = DateUtils.stampToDate(item.getCreateDate());
         helper.setText(R.id.text_time, time);
 
+        TagView view = helper.getView(R.id.tagView);
         switch (item.getDistributionFlag()) {
             case "0":
                 helper.setText(R.id.text_state, "待配送");
+                view.setColor(Color.BLUE);
                 break;
             case "1":
                 helper.setText(R.id.text_state, "已配送");
+                view.setColor(Color.GREEN);
                 break;
         }
 
